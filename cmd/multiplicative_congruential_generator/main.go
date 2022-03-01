04ca2592de58e6ca30a69ea61756ab6cbcdf80d1
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-01 16:09:47 -0600

    Add expected period for multiplicative congruential generator

--- cmd/multiplicative_congruential_generator/main.go
+++ cmd/multiplicative_congruential_generator/main.go
@@ -13,16 +13,24 @@ func main() {
 	m := readIntOrExit("Enter m: ", "invalid \"m\" value")
 
 	var expectedPeriod int
-	// If number is base 2
-	if math.Log2(float64(m)) == math.Floor(float64(m)) {
+	if (m & (m - 1)) == 0 {
+		// If number is base 2
 		expectedPeriod = m / 4
 	} else if math.Log10(float64(m)) == math.Floor(float64(m)) {
+		// If number is base 10
+		exponent := math.Log10(float64(m))
+		if exponent >= 5 {
+			expectedPeriod = int(5 * math.Pow(10, exponent-2))
+		} else {
+			expectedPeriod = int((math.Pow(5, exponent-1)) * 4)
+		}
 		expectedPeriod = m
 	}
+	fmt.Printf("Periodo esperado: %v\n", expectedPeriod)
 	x0Aux := x0
 	fmt.Println("\n n\tx0\t(ax0)modm\txn+1\tNúmeros rectangulares")
 	var n int
-	for n = 1; n <= m; n++ {
+	for n = 1; n <= expectedPeriod; n++ {
 		multiplication, x1, rectangularNumber := pkg.MultiplicativeCongruentialGeneration(a, x0Aux, 0, m)
 		intPart := multiplication / m
 		residueNumerator := multiplication - (intPart * m)
@@ -39,6 +47,7 @@ func main() {
 	}
 
 }
+
 func readIntOrExit(prompt string, errorMessage string) int {
 	fmt.Print(prompt)
 	var value int
