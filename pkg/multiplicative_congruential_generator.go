caafcd3e4b6e0c8c859d8171767fc44e175d6da7
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-01 16:10:25 -0600

    Fix named return value of MultiplicativeCongruentialGenerator

--- pkg/multiplicative_congruential_generator.go
+++ pkg/multiplicative_congruential_generator.go
@@ -5,6 +5,6 @@ func Multiplication(a int, x0 int, _ int) int {
 }
 
 // MultiplicativeCongruentialGeneration generates a new rectangular number.
-func MultiplicativeCongruentialGeneration(a int, x0 int, c int, m int) (addition int, x1 int, rectangularNumber float64) {
+func MultiplicativeCongruentialGeneration(a int, x0 int, c int, m int) (multiplication int, x1 int, rectangularNumber float64) {
 	return Multiplication(a, x0, 0), X1(a, x0, c, m, Multiplication), RectangularNumber(a, x0, c, m, Multiplication)
 }
